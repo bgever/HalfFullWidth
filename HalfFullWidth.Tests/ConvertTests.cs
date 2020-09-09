e24fe734c884a77123e7709945cf60ab39979350
Bart Verkoeijen <bart.verkoeijen@gmail.com>
2020-09-09 22:35:28 +0800

    Add Katakana alphabet test

--- HalfFullWidth.Tests/ConvertTests.cs
+++ HalfFullWidth.Tests/ConvertTests.cs
@@ -14,6 +14,7 @@ namespace HalfFullWidth.Tests
         [DataRow("ﾊﾛｰﾜｰﾙﾄﾞ", "ハローワールド")] // Hello world!
         [DataRow("ｴﾘｯｸ", "エリック")] // Eric
         [DataRow("｡｢｣､", "。「」、")] // Halfwidth CJK punctuation
+        [DataRow(" ", "　")] // Ideographic space.
         [DataRow("\u0020", "\u3000")] // Ideographic space.
         [DataRow(
             "･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ",
@@ -23,6 +24,7 @@ namespace HalfFullWidth.Tests
             "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
             "！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～"
         )] // Fullwidth ASCII variants
+        [DataRow(KATAKANA_ALPHABET_HALF, KATAKANA_ALPHABET_FULL)]
         public void ToFullWidthString(string input, string expected)
         {
             string output = input.ToFullWidthString();
@@ -41,6 +43,7 @@ namespace HalfFullWidth.Tests
         [DataRow("エリック", "ｴﾘｯｸ")] // Eric
         // --- Next DataRows are based on https://www.unicode.org/charts/PDF/UFF00.pdf ---
         [DataRow("。「」、", "｡｢｣､")] // Halfwidth CJK punctuation
+        [DataRow("　", " ")] // Ideographic space.
         [DataRow("\u3000", "\u0020")] // Ideographic space.
         [DataRow(
             "・ヲァィゥェォャュョッーアイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワン\u3099\u309A",
@@ -50,6 +53,7 @@ namespace HalfFullWidth.Tests
             "！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～",
             "!\"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
         )] // Fullwidth ASCII variants
+        [DataRow(KATAKANA_ALPHABET_FULL, KATAKANA_ALPHABET_HALF)]
         public void ToHalfWidthString(string input, string expected)
         {
             string output = Convert.ToHalfWidthString(input);
@@ -60,16 +64,60 @@ namespace HalfFullWidth.Tests
         const string MIXED_TO_FULL = "ＴＨＥ\u3000ｑｕｉｃｋ，\u3000ＢＲＯＷＮ\u3000ｆｏｘ．";
         const string MIXED_TO_HALF = "THE quick, BROWN fox.";
 
+        [TestMethod]
         public void ToFullWidthString_Mixed()
         {
             string output = MIXED.ToFullWidthString();
             Assert.AreEqual(MIXED_TO_FULL, output);
         }
 
+        [TestMethod]
         public void ToHalfWidthString_Mixed()
         {
             string output = MIXED.ToHalfWidthString();
             Assert.AreEqual(MIXED_TO_HALF, output);
         }
+
+        const string KATAKANA_ALPHABET_HALF = @"
+               ｱ a    ｲ i    ｳ u    ｴ e    ｵ o
+            K  ｶ ka   ｷ ki   ｸ ku   ｹ ke   ｺ ko   ｷｬ kya  ｷｭ kyu  ｷｮ kyo
+            S  ｻ sa   ｼ shi  ｽ su   ｾ se   ｿ so   ｼｬ sha  ｼｭ shu  ｼｮ sho
+            T  ﾀ ta   ﾁ chi  ﾂ tsu  ﾃ te   ﾄ to   ﾁｬ cha  ﾁｭ chu  ﾁｮ cho
+            N  ﾅ na   ﾆ ni   ﾇ nu   ﾈ ne   ﾉ no   ﾆｬ nya  ﾆｭ nyu  ﾆｮ nyo
+            H  ﾊ ha   ﾋ hi   ﾌ fu   ﾍ he   ﾎ ho   ﾋｬ hya  ﾋｭ hyu  ﾋｮ hyo
+            M  ﾏ ma   ﾐ mi   ﾑ mu   ﾒ me   ﾓ mo   ﾐｬ mya  ﾐｭ myu  ﾐｮ myo
+            Y  ﾔ ya          ﾕ yu          ﾖ yo
+            R  ﾗ ra   ﾘ ri   ﾙ ru   ﾚ re   ﾛ ro   ﾘｬ rya  ﾘｭ ryu  ﾘｮ ryo
+            W  ﾜ wa
+               ﾝ n
+            G  ｶﾞ ga   ｷﾞ gi   ｸﾞ gu   ｹﾞ ge   ｺﾞ go   ｷﾞｬ gya  ｷﾞｭ guy  ｷﾞｮ gyo
+            Z  ｻﾞ za   ｼﾞ ji   ｽﾞ zu   ｾﾞ ze   ｿﾞ zo   ｼﾞｬ ja   ｼﾞｭ ju   ｼﾞｮ jo
+            D  ﾀﾞ da   ﾁﾞ (ji) ﾂﾞ (zu) ﾃﾞ de   ﾄﾞ do
+            B  ﾊﾞ ba   ﾋﾞ bi   ﾌﾞ bu   ﾍﾞ be   ﾎﾞ bo   ﾋﾞｬ bya  ﾋﾞｭ byu  ﾋﾞｮ byo
+            P  ﾊﾟ pa   ﾋﾟ pi   ﾌﾟ pu   ﾍﾟ pe   ﾎﾟ po   ﾋﾟｬ pya  ﾋﾟｭ pyu  ﾋﾟｮ pyo
+            F  ﾌｧ fa  ﾌｨ fi         ﾌｪ fe  ﾌｫ fo
+            T  ﾂｧ tsa ﾃｨ ti  ﾄｩ tu
+            W                       ｳｪ we  ｳｫ wo";
+
+        const string KATAKANA_ALPHABET_FULL = @"
+　　　　　　　　　　　　　　　ア　ａ　　　　イ　ｉ　　　　ウ　ｕ　　　　エ　ｅ　　　　オ　ｏ
+　　　　　　　　　　　　Ｋ　　カ　ｋａ　　　キ　ｋｉ　　　ク　ｋｕ　　　ケ　ｋｅ　　　コ　ｋｏ　　　キャ　ｋｙａ　　キュ　ｋｙｕ　　キョ　ｋｙｏ
+　　　　　　　　　　　　Ｓ　　サ　ｓａ　　　シ　ｓｈｉ　　ス　ｓｕ　　　セ　ｓｅ　　　ソ　ｓｏ　　　シャ　ｓｈａ　　シュ　ｓｈｕ　　ショ　ｓｈｏ
+　　　　　　　　　　　　Ｔ　　タ　ｔａ　　　チ　ｃｈｉ　　ツ　ｔｓｕ　　テ　ｔｅ　　　ト　ｔｏ　　　チャ　ｃｈａ　　チュ　ｃｈｕ　　チョ　ｃｈｏ
+　　　　　　　　　　　　Ｎ　　ナ　ｎａ　　　ニ　ｎｉ　　　ヌ　ｎｕ　　　ネ　ｎｅ　　　ノ　ｎｏ　　　ニャ　ｎｙａ　　ニュ　ｎｙｕ　　ニョ　ｎｙｏ
+　　　　　　　　　　　　Ｈ　　ハ　ｈａ　　　ヒ　ｈｉ　　　フ　ｆｕ　　　ヘ　ｈｅ　　　ホ　ｈｏ　　　ヒャ　ｈｙａ　　ヒュ　ｈｙｕ　　ヒョ　ｈｙｏ
+　　　　　　　　　　　　Ｍ　　マ　ｍａ　　　ミ　ｍｉ　　　ム　ｍｕ　　　メ　ｍｅ　　　モ　ｍｏ　　　ミャ　ｍｙａ　　ミュ　ｍｙｕ　　ミョ　ｍｙｏ
+　　　　　　　　　　　　Ｙ　　ヤ　ｙａ　　　　　　　　　　ユ　ｙｕ　　　　　　　　　　ヨ　ｙｏ
+　　　　　　　　　　　　Ｒ　　ラ　ｒａ　　　リ　ｒｉ　　　ル　ｒｕ　　　レ　ｒｅ　　　ロ　ｒｏ　　　リャ　ｒｙａ　　リュ　ｒｙｕ　　リョ　ｒｙｏ
+　　　　　　　　　　　　Ｗ　　ワ　ｗａ
+　　　　　　　　　　　　　　　ン　ｎ
+　　　　　　　　　　　　Ｇ　　ガ　ｇａ　　　ギ　ｇｉ　　　グ　ｇｕ　　　ゲ　ｇｅ　　　ゴ　ｇｏ　　　ギャ　ｇｙａ　　ギュ　ｇｕｙ　　ギョ　ｇｙｏ
+　　　　　　　　　　　　Ｚ　　ザ　ｚａ　　　ジ　ｊｉ　　　ズ　ｚｕ　　　ゼ　ｚｅ　　　ゾ　ｚｏ　　　ジャ　ｊａ　　　ジュ　ｊｕ　　　ジョ　ｊｏ
+　　　　　　　　　　　　Ｄ　　ダ　ｄａ　　　ヂ　（ｊｉ）　ヅ　（ｚｕ）　デ　ｄｅ　　　ド　ｄｏ
+　　　　　　　　　　　　Ｂ　　バ　ｂａ　　　ビ　ｂｉ　　　ブ　ｂｕ　　　ベ　ｂｅ　　　ボ　ｂｏ　　　ビャ　ｂｙａ　　ビュ　ｂｙｕ　　ビョ　ｂｙｏ
+　　　　　　　　　　　　Ｐ　　パ　ｐａ　　　ピ　ｐｉ　　　プ　ｐｕ　　　ペ　ｐｅ　　　ポ　ｐｏ　　　ピャ　ｐｙａ　　ピュ　ｐｙｕ　　ピョ　ｐｙｏ
+　　　　　　　　　　　　Ｆ　　ファ　ｆａ　　フィ　ｆｉ　　　　　　　　　フェ　ｆｅ　　フォ　ｆｏ
+　　　　　　　　　　　　Ｔ　　ツァ　ｔｓａ　ティ　ｔｉ　　トゥ　ｔｕ
+　　　　　　　　　　　　Ｗ　　　　　　　　　　　　　　　　　　　　　　　ウェ　ｗｅ　　ウォ　ｗｏ";
     }
 }
